790d0c34a508d4b9b5c6b7752cb6628d36dad1d9
Dennis Winter <dennis.winter@avanade.com>
2022-06-22 00:56:35 +0200

    added content

--- Terraform/module_and_depends_on/main.tf
+++ Terraform/module_and_depends_on/main.tf
@@ -2,8 +2,17 @@ provider "azurerm" {
     features {}
 }
 
-resource "azurerm_resource_group" "Step1" {
-    name     = "Step1"
+resource "azurerm_resource_group" "rg" {
+    name     = "rg"
     location = "West Europe"
 #    tags     = { A = "B" }
+}
+
+module "network" {
+    source                  = "./network_w_dataSource_modified"    # We are back to data sources - see within the modified module
+    depends_on              = [azurerm_resource_group.rg]
+
+    vnet_name               = "vnet"
+    resource_group_name     = azurerm_resource_group.rg.name
+    address_space           = [ "10.0.0.0/16" ]
 }
--- Terraform/module_and_depends_on/network_w_dataSource_modified/main.tf
+++ Terraform/module_and_depends_on/network_w_dataSource_modified/main.tf
@@ -1,7 +1,7 @@
 
 
 locals {
-    identifier = format( "%s", var.resource_group_name )
+    identifier = format( "%s%s", var.resource_group_namem, data.azurerm_resource_group.network.location )
 }
 
 data "azurerm_resource_group" "network" {
@@ -12,10 +12,10 @@ data "azurerm_client_config" "current" {
 }
 
 resource "azurerm_virtual_network" "vnet" {
-#    lifecycle {
-#        ignore_changes = [ location, resource_group_name ]
-#    }
-#    for_each = { ( local.identifier ) =  1 }
+    lifecycle {
+        ignore_changes = [ location, resource_group_name ]
+    }
+    for_each = { ( local.identifier ) =  1 }
     name                = var.vnet_name
     resource_group_name = data.azurerm_resource_group.network.name
     location            = data.azurerm_resource_group.network.location
